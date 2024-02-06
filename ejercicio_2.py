1a05634ef280e2bf045b637676790d3e53fd332b
saori10 <saori.rg09@gmail.com>
2024-02-05 20:28:07 -0500

    i did the second exercise

---
+++ ejercicio_2.py
@@ -0,0 +1,6 @@
+password = "pepito123"
+user_pass = input("Introduce la contraseña: ")
+if password.lower() == user_pass.lower() :
+    print("Las contraseñas coinciden")
+else:
+    print("No coinciden las contraseñas")
