991d5a2530e76d9bb7c0601cb62d64d2750074e2
saori10 <saori.rg09@gmail.com>
2024-02-05 20:41:15 -0500

    i did the third exercise

---
+++ ejercicio_3.py
@@ -0,0 +1,8 @@
+number_1 = float(input("Ingrese un número: "))
+number_2 = float(input("Ingrese un segundo número: "))
+
+if number_2 != 0:
+    division = number_1 / number_2
+    print("La division de esos números es ", division)
+else: 
+    print("Error")
