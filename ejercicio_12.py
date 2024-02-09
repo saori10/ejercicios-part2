17cb21de59a0a8cb4ee6bde17738d17ef73161c5
saori10 <saori.rg09@gmail.com>
2024-02-08 19:52:23 -0500

    i did the twelfth exercise

---
+++ ejercicio_12.py
@@ -0,0 +1,6 @@
+subjects = ["Matemáticas", "Física", "Química", "Historia", "Lengua"]
+print("Yo estudio ",subjects[0])
+print("Yo estudio ",subjects[1])
+print("Yo estudio ",subjects[2])
+print("Yo estudio ",subjects[3])
+print("Yo estudio ",subjects[4])
