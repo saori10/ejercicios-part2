3f77cd19a1b12afdc7736599fd622a1dfca5f5c2
saori10 <saori.rg09@gmail.com>
2024-02-15 20:05:24 -0500

    i did the exercise number 18

--- ejercicio_18.py
+++ ejercicio_18.py
@@ -2,8 +2,8 @@ word = input("Ingrese una palabra: ")
 word_palindrome = word
 word = list(word)
 word_palindrome = list(word_palindrome)
-word_palindrome.reverse()
 
+word_palindrome.reverse()
 
 if word_palindrome == word:
     print("Es palíndromo")
